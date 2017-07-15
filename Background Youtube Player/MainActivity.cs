c2d9ae53f9fb0fc0125c14509183d8c8012396af
Bubnjarko-PC\Bubnjarko <darko.jukic1991@gmail.com>
2017-07-15 23:04:26 +0200

    Added drawer navigation view. (Not yet functional). Added app icon.

--- Background Youtube Player/MainActivity.cs
+++ Background Youtube Player/MainActivity.cs
@@ -11,15 +11,23 @@ using System.Net;
 using Newtonsoft.Json;
 using Android.Views;
 using Android.Content;
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.V4.View;
 
 namespace Background_Youtube_Player
 {
-    [Activity(Label = "Darkov Youtube Player", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Darkov Youtube Player", MainLauncher = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class MainActivity : AppCompatActivity
     {
         MediaPlayer player;
         SearchView songSearchView;
         Toolbar toolbar;
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+
         ListView songListView;
         string tag = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=AIzaSyADs8hX9blKmzfBRkVGxLcQhRdMB80qBTc&q=";
 
@@ -67,13 +75,51 @@ namespace Background_Youtube_Player
             toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             toolbar.InflateMenu(Resource.Menu.toolbar_menu);
             toolbar.SetTitle(Resource.String.ToolbarTitle);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.DrawerLayout);
+            navigationView = FindViewById<NavigationView>(Resource.Id.NavigationView);
+            navigationView.SetCheckedItem(Resource.Id.nav_home);
 
             toolbar.MenuItemClick += (sender, e) => {
                 if(notificationManager != null)
                 notificationManager.CancelAll();
                 player.Stop();
             };
+
+            SetSupportActionBar(toolbar);
+
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+            navigationView.NavigationItemSelected += (sender, e) => {
+                e.MenuItem.SetChecked(true);
+                //react to click here and swap fragments or navigate
+                drawerLayout.CloseDrawers();
+            };
+
+
             songSearchView = FindViewById<SearchView>(Resource.Id.songSearchView);
+        }
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+
+                    if (!drawerLayout.IsDrawerOpen(GravityCompat.Start))
+                    {
+                        drawerLayout.OpenDrawer(GravityCompat.Start);
+                        return true;
+                    }
+                    else
+                    {
+                        drawerLayout.CloseDrawer(GravityCompat.Start);
+                        return true;
+                    }
+
+            }
+            return base.OnOptionsItemSelected(item);
         }
 
         private async Task SearchForSong(object sender, SearchView.QueryTextSubmitEventArgs e)
@@ -128,7 +174,7 @@ namespace Background_Youtube_Player
                 {
                     await DecryptDownloadUrlAsync(video);
                     Toast.MakeText(this, "Cannot play this video", ToastLength.Short);
-                    GetHttpResponse(video.DownloadUrl, video);
+                    await GetHttpResponse(video.DownloadUrl, video);
                 }
                 else
                 {
